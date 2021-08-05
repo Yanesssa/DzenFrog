60096578169dd78f84e0efbdedc1b5321c5fbe4d
Yanessa <yanessa_n@mail.ru>
2021-08-05 20:28:12 +0300

    Add adaptive for table page

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,14 @@
+const body = document.querySelector('.body');
+const descriptionButton = document.querySelector('.description_button');
+const popup = document.querySelector('.popup');
 
+descriptionButton.addEventListener('click', function(){
+  popup.classList.add('popup_active');
+  body.classList.add('popup_active');
+  popup.addEventListener('click', function(){
+    popup.classList.remove('popup_active');
+    body.classList.remove('popup_active');
+  });
+});
 
 
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,6 @@
 const headerButton = document.querySelector('.header_button');
 const header = document.querySelector('.header');
-const body = document.querySelector('.body');
-const descriptionButton = document.querySelector('.description_button');
-const popup = document.querySelector('.popup');
 
 headerButton.addEventListener('click', function(){
   header.classList.toggle('nav_opened');
-});
-
-descriptionButton.addEventListener('click', function(){
-  popup.classList.add('popup_active');
-  body.classList.add('popup_active');
-  popup.addEventListener('click', function(){
-    popup.classList.remove('popup_active');
-    body.classList.remove('popup_active');
-  });
 });
